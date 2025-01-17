b2613f4c956db5b3e6a7d118fa4e8e09b060a07c
lazer_007 <l.baxtiyorov@newuu.uz>
2025-01-17 10:39:29 +0500

    Age asking

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int age;
+    cout << "Hello, How old are you?" << endl;
+    cout << "Your age:";
+    cin >> age;
+    cout <<"So, you age:"<<age<< endl;
+
+    return 0;
+
+}
